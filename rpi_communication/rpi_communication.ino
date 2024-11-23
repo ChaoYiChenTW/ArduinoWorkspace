7bc946402b431f931ce28788b7cefdecc6f5dfa9
Chao <a951159a@gmail.com>
2024-11-23 22:48:18 +0000

    Debug

--- rpi_communication/rpi_communication.ino
+++ rpi_communication/rpi_communication.ino
@@ -1,8 +1,11 @@
 void setup() {
-    Serial.begin(9600);
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
 }
 
 void loop() {
-    Serial.println("Hello, RPi!");
-    delay(1000);
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello from Arduino!");
+  delay(1000);
 }
